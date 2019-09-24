1b949a50c36dd452d69e5e9fc1b9b80987daa241
Andrew Klaas <aklaas@hashicorp.com>
2019-09-24 14:41:13 -0500

    fixing script

--- identity/ssh-ca/vagrant-local/2_client_setup.sh
+++ identity/ssh-ca/vagrant-local/2_client_setup.sh
@@ -6,7 +6,6 @@ ssh-keygen -f /home/vagrant/.ssh/id_rsa -t rsa -N ''
 # Authenticate to Vault
 vault login -method=userpass username=johnsmith password=test
 
-sudo rm /home/vagrant/.ssh/id_rsa-cert.pub
 cat /home/vagrant/.ssh/id_rsa.pub | \
   vault write -format=json ssh-client-signer/sign/clientrole valid_principals=johnsmith public_key=- \
   | jq -r '.data.signed_key' > /home/vagrant/.ssh/id_rsa-cert.pub
@@ -14,5 +13,4 @@ cat /home/vagrant/.ssh/id_rsa.pub | \
 sudo chmod 0400 /home/vagrant/.ssh/id_rsa-cert.pub
 
 echo "To use the new cert you can use the following command"
-echo "ssh vault"
-#ssh -i /home/vagrant/.ssh/id_rsa -i /home/vagrant/.ssh/id_rsa-cert.pub johnsmith@vault
+echo "ssh -i /home/vagrant/.ssh/id_rsa -i /home/vagrant/.ssh/id_rsa-cert.pub johnsmith@vault"
